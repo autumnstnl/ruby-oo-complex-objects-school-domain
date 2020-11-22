07986ead1329417c6c70f7598ba77a0570429072
Autumn <autumnstnl@gmail.com>
2020-11-22 00:02:32 -0500

    commit tree 4326545dac2539e1c619ad55dd021da5848dab63

--- lib/school.rb
+++ lib/school.rb
@@ -1,1 +1,32 @@
-# code here!
+class  School
+attr_accessor :name, :school, :roster
+
+    def initialize(school)
+        @school = school
+        @roster = {}
+    end
+
+    def add_student(name, grade)
+        if @roster[grade]
+           @roster[grade] << name
+        else 
+            @roster[grade] = [name]
+        end
+    end
+
+    def grade(number)
+        @roster.select do |grade, names|
+            if number == grade
+               return names
+            end 
+        end
+    end
+
+    def sort
+        hash_roster = {}
+        @roster.each do |grade, names|
+            hash_roster[grade] = names.sort!
+        end
+        hash_roster
+    end
+end
